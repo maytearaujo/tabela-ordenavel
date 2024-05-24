49078d6bf4d3c9e3281188e445d4b7263b5bfffa
Maytê <maytearaujo@yahoo.com.br>
2024-05-23 23:41:43 -0300

    feat: First Commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+
+const SearchBar = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Pesquisar..." 
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="search-bar"
+    />
+  );
+};
+
+export default SearchBar;
